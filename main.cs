3f9fb6e9c7667e094611a5d7b9ca87b9e656db58
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2023-01-23 17:33:46 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,31 @@
+// Created by: Ashley Monaghan
+// Created on: Jan 2023
+//
+// This program shows pictures of dogs!
+
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+class Program
+{
+    public static async Task Main()
+    {
+        HttpClient client = new HttpClient();
+        string response = await client.GetStringAsync(
+            "https://dog.ceo/api/breeds/image/random"
+        );
+        var jsonAsDictionary = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        Console.WriteLine("");
+        JsonNode docNode = JsonNode.Parse(response)!;
+        JsonNode factsNode = docNode!["message"]!;
+        string fact = factsNode.ToString();
+        Console.WriteLine(fact);
+        
+        Console.WriteLine("\nDone.");
+    }
+}
